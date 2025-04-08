6acaa56da5193757fc2ef0da33597cacfd76a487
Yona Bystedt <yona.bystedt@vertigis.com>
2025-04-08 09:09:48 -0700

    add workaround for bug in globals

--- config/eslint.base.config.js
+++ config/eslint.base.config.js
@@ -1,6 +1,6 @@
-
 // @ts-check
 /* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
 "use strict";
 import eslint from "@eslint/js";
 import tseslint from "typescript-eslint";
@@ -15,6 +15,17 @@ import globals from "globals";
 // This "plugin" actually works by monkeypatching an eslint method.
 import "eslint-plugin-only-warn";
 
+// Versions of the 'globals' package prior to v13.12.1 have an extra space in a
+// key that crashes eslint (https://github.com/sindresorhus/globals/pull/184).
+// As Web itself (5.34) only demands v11.2 and SDK projects don't have an
+// explicit dependency, the wrong package can sometimes end up being used.
+const browserGlobals = Object.keys(globals.browser).reduce((acc, key) => {
+    // @ts-expect-error: It's fine, the keys are all strings.
+    acc[key.trim()] = globals.browser[key];
+    return acc;
+}, {});
+const eslintGlobals = { ...globals, browser: browserGlobals };
+
 /**
  * A default eslint configuration that can be extended.
  */
@@ -24,8 +35,8 @@ const baseConfig = defineConfig([
         name: "vertigis/recommended",
         languageOptions: {
             globals: {
-                ...globals.browser,
-                ...globals.node,
+                ...eslintGlobals.browser,
+                ...eslintGlobals.node,
             },
             ecmaVersion: 2022,
             sourceType: "module",
